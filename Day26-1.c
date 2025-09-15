8a28b90b6caefc5d0dc2291a8675e328bb879e19
Prarthana Baisoya <baisoyaprarthana19@gmail.com>
2025-09-15 10:16:27 +0530

    Add files via upload

---
+++ Day26-1.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+    int i, j;
+    int n = 5; 
+
+    
+    for (i = 1; i <= n; i++) {
+        
+        for (j = 1; j <= n - i; j++) {
+            printf(" ");
+        }
+
+        
+        for (j = n - i + 1; j <= n; j++) {
+            printf("%d", j);
+        }
+
+        printf("\n"); 
+    }
+
+    return 0;
+}
