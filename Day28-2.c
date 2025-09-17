fdf6cafbfdcb2d7a9691f1e33cc4d59440bc9624
Prarthana Baisoya <baisoyaprarthana19@gmail.com>
2025-09-17 19:23:14 +0530

    Add files via upload

---
+++ Day28-2.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main() {
+    int n, i;
+
+    
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    int arr[n]; 
+
+    
+    printf("Enter %d elements:\n", n);
+    for (i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    
+    printf("Array elements are:\n");
+    for (i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+
+    printf("\n");
+    return 0;
+}
