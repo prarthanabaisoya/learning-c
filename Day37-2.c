c67e01dd2d9fc20be9a41d876b554244b82b6b90
Prarthana Baisoya <baisoyaprarthana19@gmail.com>
2025-09-26 12:07:32 +0530

    Add files via upload

---
+++ Day37-2.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main() {
+    int m, n;
+    scanf("%d %d", &m, &n);   // Input rows and columns
+    
+    int matrix[m][n], transpose[n][m];
+    
+    // Input matrix
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
+            scanf("%d", &matrix[i][j]);
+        }
+    }
+    
+    // Compute transpose
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
+            transpose[j][i] = matrix[i][j];
+        }
+    }
+    
+    // Print transpose
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            printf("%d ", transpose[i][j]);
+        }
+        printf("\n");
+    }
+    
+    return 0;
+}
